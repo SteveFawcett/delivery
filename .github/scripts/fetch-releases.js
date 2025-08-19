b566220b34343640669178292db1b628e419ce10
Steven Fawcett <64777084+sfawcett123@users.noreply.github.com>
2025-08-19 18:26:13 +0100

    Create fetch-releases.js

---
+++ .github/scripts/fetch-releases.js
@@ -0,0 +1,44 @@
+// .github/scripts/fetch-releases.js
+const axios = require('axios');
+const fs = require('fs');
+const repos = [
+  'SteveFawcett/TestPlugin',
+  'SteveFawcett/RedisPlugin',
+  'SteveFawcett/MSFSPlugin',
+  'SteveFawcett/LocalCachePlugin',
+  'SteveFawcett/BroadcastPluginSDK',
+  'SteveFawcett/APIPlugin'
+];
+
+const headers = {
+  Authorization: `Bearer ${process.env.GH_TOKEN}`,
+  'User-Agent': 'release-fetcher'
+};
+
+(async () => {
+  const allReleases = [];
+
+  for (const repo of repos) {
+    const url = `https://api.github.com/repos/${repo}/releases`;
+    const { data } = await axios.get(url, { headers });
+
+    data.forEach(release => {
+      const zipAssets = release.assets
+        .filter(a => a.name.endsWith('.zip'))
+        .map(a => ({
+          name: a.name,
+          url: a.browser_download_url
+        }));
+
+      allReleases.push({
+        repo,
+        tag: release.tag_name,
+        name: release.name,
+        published: release.published_at,
+        zipFiles: zipAssets
+      });
+    });
+  }
+
+  fs.writeFileSync('releases.json', JSON.stringify(allReleases, null, 2));
+})();
